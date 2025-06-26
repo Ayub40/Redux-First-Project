d2a72ec09604931bbf4b6bb185dfe42bbfb9c2b5
Ayub40 <ayubk4028@gmail.com>
2025-06-27 03:21:52 +0600

    22-2 Understanding mutation and ways to avoid it.

--- test.js
+++ test.js
@@ -1,29 +1,72 @@
+// 22-1 Basics of Functional Programming.
 // Pure function
-
-const add = (a, b) => a + b;
+// Example --> 1
+// const add = (a, b) => a + b;
 
 // console.log(add(0, 1));
 // console.log(add(1, 1));
 // console.log(add(2, 1));
 
+// Example --> 2
 // Impure function
 // let total = 0;
 // const addTotal = (amount) => (total = total + amount);
 
 // console.log(addTotal(4));
 
-const updateDate = () => {
-    new Date();
-}
+// Example --> 3
 
-const randomNumber = (amount) => {
-    return amount + Math.random();
+// const updateDate = () => {
+//     new Date();
+// }
+
+// const randomNumber = (amount) => {
+//     return amount + Math.random();
+// };
+
+// console.log(randomNumber(3));
+// console.log(randomNumber(3));
+// console.log(randomNumber(3));
+// console.log(randomNumber(3));
+
+// ===========================================================
+// ===========================================================
+// ===========================================================
+
+//  22-2 Understanding mutation and ways to avoid it.
+import { produce } from "immer";
+
+const employee = {
+    name: "Mir",
+    address: { country: "Bangladesh", city: "Dhaka" },
 };
 
-console.log(randomNumber(3));
-console.log(randomNumber(3));
-console.log(randomNumber(3));
-console.log(randomNumber(3));
+// ------------------------------------
+// const employee2 = employee;
+// employee2.name = "Mezba";
+// console.log(employee == employee2);
+
+// const employee2 = {
+//     ...employee,
+//     name: "Mezba",
+//     address: { ...employee.address, city: "Chittagong" },
+// };
+
+//  eta kora jabe na
+// employee2.address.city = "Chittagong";     
+// ------------------------------------
+
+
+// Using Immar
+const employee2 = produce(employee, (draft) => {
+    draft.name = "Mezba";
+    draft.address.city = "Chittagong";
+});
+
+
+// console.log(employee == employee2);
+console.log(employee);
+console.log(employee2);
 
 
 
